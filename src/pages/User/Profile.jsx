f678ea841534323ee80288a4b7a4ce3fe2404a18
elijah571 <elijahfx43@gmail.com>
2025-02-26 18:15:30 +0100

    Fix: Corrected import path for Header component

--- src/pages/User/Profile.jsx
+++ src/pages/User/Profile.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
-import Loader from "../../components/Loader";
+import Loader from "../../Components/Loader";
+
 import { useProfileMutation } from "../../redux/api/usersApiSlice";
 import { setCredentials } from "../../redux/features/auth/authSlice";
 import { Link } from "react-router-dom";
