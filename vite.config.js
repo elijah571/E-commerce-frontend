94b9b2e50af69500dee64303b5255aab2b39601d
elijah571 <elijahfx43@gmail.com>
2025-02-26 18:34:23 +0100

    Fix: package.json

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/": "https://e-commer-backend-ew19.onrender.com",
-      
-      
-      "/uploads/":   "https://e-commer-backend-ew19.onrender.com",
-      
+      "/uploads/": "https://e-commer-backend-ew19.onrender.com",
     },
-    allowedHosts: ['https://e-commerce-frontend-dg0q.onrender.com]
+    allowedHosts: ["https://e-commerce-frontend-dg0q.onrender.com"],
   },
 });
